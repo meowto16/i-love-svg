b5f9124dd0c7e3d6d8a984e997b0ed5d38049dde
Maxim Kirshin <meowto16@gmail.com>
2022-03-17 00:18:54 +0500

    2. Adds cat-runner

--- src/App.js
+++ src/App.js
@@ -5,16 +5,18 @@ import classes from './App.module.css'
 const Heart = React.lazy(() => import('./experiments/heart'))
 const VK = React.lazy(() => import('./experiments/vk'))
 const AnatomyOfSvg = React.lazy(() => import('./experiments/anatomy-of-svg'))
+const CatRunner = React.lazy(() => import('./experiments/cat-runner'))
 
 const SvgList = {
   'heart': Heart,
   'vk': VK,
   'anatomy-of-svg': AnatomyOfSvg,
+  'cat-runner': CatRunner,
   'none': () => <>Выберите SVG</>
 }
 
 const App = () => {
-  const [activeSvg, setActiveSvg] = useState('anatomy-of-svg')
+  const [activeSvg, setActiveSvg] = useState('cat-runner')
 
   const ActiveSvgComponent = SvgList[activeSvg]
 
---
+++ src/experiments/cat-runner.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import classes from './cat-runner.module.css'
+
+const CatRunner = () => {
+  return <div className={classes.catRunner} />
+}
+
+export default CatRunner
